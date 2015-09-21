1f72f8e4d42ea1b7628bd990ef70dbd629e9b6ef
Korben Zhang <korbenzhang@gmail.com>
2015-09-21 12:14:14 +0800

    fix lib

--- writer.go
+++ writer.go
@@ -1,8 +1,7 @@
 package mcsv
 
-
 import (
-	"github.com/github.com/mabetle/mcore"
+	"github.com/mabetle/mcore"
 	"github.com/mabetle/mlog"
 )
 
@@ -10,34 +9,33 @@ var (
 	logger = mlog.GetLogger("github.com/mabetle/mcsv")
 )
 
-func (csv CSV) Write(file string){
+func (csv CSV) Write(file string) {
 
 }
 
 // WriteData
-func WriteData(location string, data[][]string)(error){
-	if location == ""{
+func WriteData(location string, data [][]string) error {
+	if location == "" {
 		location = "/rundata/export.csv"
 	}
-	content:=BuildCsvContent(data)
-	_, err:= mcore.WriteFile(location, content)
-	logger.Debug("Write CSV data to :", location, ", Result: " , err)
+	content := BuildCsvContent(data)
+	_, err := mcore.WriteFile(location, content)
+	logger.Debug("Write CSV data to :", location, ", Result: ", err)
 	return err
 }
 
-func BuildCsvContent(data [][]string)(r string){
+func BuildCsvContent(data [][]string) (r string) {
 	rows := len(data)
 	cols := len(data[0])
-	for row := 0; row<rows; row++{
-		for col:=0;col<cols;col++{
-			if col == cols - 1{
-				r=r + data[row][col]
-			}else{
-				r=r + data[row][col] + ","
+	for row := 0; row < rows; row++ {
+		for col := 0; col < cols; col++ {
+			if col == cols-1 {
+				r = r + data[row][col]
+			} else {
+				r = r + data[row][col] + ","
 			}
 		}
 		r = r + "\n"
 	}
 	return
 }
-
